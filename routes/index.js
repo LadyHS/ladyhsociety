c0aaeceede29dc0bce2899f05ba81123ff8811e9
leaikkache <12200986+leaikkache@users.noreply.github.com>
2022-05-01 19:15:41 +0200

    creation of the website

--- routes/index.js
+++ routes/index.js
@@ -12,25 +12,20 @@ router.get("/", function (req, res, next) {
   });
 });
 
-router.get(["/prestations", "/services"], function (req, res, next) {
-  res.render("presta", {
-    styles: ["header", "presta", "buttons", "forms"],
-    scripts: ["presta"],
-    currentPage: "presta",
+router.get(["/manifeste"], function (req, res, next) {
+  res.render("manifeste", {
+    styles: ["header","home", "buttons", "forms"],
+    scripts: ["manifeste"],
+    currentPage: "manifeste",
   });
 });
 
-router.get(["/transition", "/sustainability"], function (req, res, next) {
-  res.render("transition", {
-    styles: ["header", "transition", "full-page-list", "buttons","presta"],
-    currentPage: "transition",
-  });
-});
 
-router.get("/about", function (req, res, next) {
-  res.render("about", {
-    styles: ["header", "about", "full-page-list","buttons"],
-    currentPage: "about",
+
+router.get("/mission", function (req, res, next) {
+  res.render("mission", {
+    styles: ["home","mission","header","about","buttons"],
+    currentPage: "mission",
   });
 });
 
@@ -42,68 +37,21 @@ router.get("/contact", function (req, res, next) {
   });
 });
 
-router.get(["/ressources", "/resources"], function (req, res, next) {
-  res.render("resources", {
-    styles: ["header","resources","full-page-list", "buttons"],
-    currentPage: "ressources",
+
+router.get(["/team"], function (req, res, next) {
+  res.render("team", {
+    styles: ["header", "home","full-page-list"],
+    currentPage: "team",
   });
 });
 
-router.get(["/legal"], function (req, res, next) {
-  res.render("legal", {
-    styles: ["header", "legal", "full-page-list"],
-    currentPage: "legal",
+router.get(["/sources1"], function (req, res, next) {
+  res.render("sources1", {
+    styles: ["header", "home","full-page-list"],
+    currentPage: "sources1",
   });
-});
+}); 
 
-// Individual invoice on presta page
-
-router.post("/send-invoice", function (req, res, next) {
-  let {
-    email,
-    name,
-    number,
-    type,
-    quantity,
-    message,
-    requestedService,
-  } = req.body;
-  let transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASSWORD,
-    },
-  });
-  transporter
-    .sendMail({
-      from: `demande devis <${email}>`,
-      to: process.env.MY_EMAIL,
-      subject: `< FREQUENCE 440 > Demande de devis individuel de ${name}`,
-      text: message,
-      html: `
-      <p><strong>Prestation demandée</strong>: ${requestedService}</p>
-      <p><strong>Nom du client</strong>: ${name}</p>
-      <p><strong>Email du client</strong>: ${email}</p>
-      <p><strong>Numéro de téléphone</strong>: ${number}</p>
-      <p><strong>Nom de l'Entreprise ou Ecole du supérieur</strong>: ${type}</p>
-      <p><strong>Nombre de personnes</strong>:${quantity}</p>
-      <p><strong>Message</strong>: ${message}</p>
-      `,
-    })
-    .then(() =>
-      res.status(200).json({
-        fr: "Votre demande a été envoyée avec succès",
-        en: "Your email has been successfully sent",
-      })
-    )
-    .catch(() =>
-      res.json({
-        fr: "Une erreur s'est produite, veuillez réessayer",
-        en: "An error occured, please try again",
-      })
-    );
-});
 
 // Contact and full invoice on contact page
 
