d4dae98c4b819d574cafd646394010c171bd4533
leaikkache <12200986+leaikkache@users.noreply.github.com>
2022-06-02 18:33:57 +0200

    delete presta form

--- routes/index.js
+++ routes/index.js
@@ -66,7 +66,7 @@ router.post("/send-contact", function (req, res, next) {
     .sendMail({
       from: `demande contact <${email}>`,
       to: process.env.MY_EMAIL,
-      subject: `< FREQUENCE 440 > Demande de contact de ${name}`,
+      subject: `< Lady H Society > Demande de contact de ${name}`,
       text: message,
       html: `
       <p><strong>Nom du client</strong>: ${name}</p>
@@ -89,59 +89,5 @@ router.post("/send-contact", function (req, res, next) {
     );
 });
 
-router.post("/send-complete-invoice", function (req, res, next) {
-  console.log(req.body);
-  let { email, name, number, clientType, address, services } = req.body;
-  let transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASSWORD,
-    },
-  });
-
-  let servicesContent = "";
-  services.forEach((service, index) => {
-    servicesContent += `
-    <div>
-      <p>Prestation numéro ${index + 1}: </p>
-      <ul>
-        <li><strong>Type de prestation</strong>: ${service.type}</li>
-        <li><strong>Nom de la prestation</strong>: ${service.name}</li>
-        <li><strong>Nombre de personnes</strong>: ${service.quantity}</li>
-        <li><strong>Message spécifique à cette prestation</strong>: ${
-          service.message
-        }</li>
-      </ul>
-    </div>`;
-  });
-  transporter
-    .sendMail({
-      from: `demande devis complet <${email}>`,
-      to: process.env.MY_EMAIL,
-      subject: `< FREQUENCE 440 > Demande de devis complet de ${name}`,
-      text: name,
-      html: `
-      <p><strong>Nom du client</strong>: ${name}</p>
-      <p><strong>Email du client</strong>: ${email}</p>
-      <p><strong>Numéro de téléphone</strong>: ${number}</p>
-      <p><strong>Nom de l'Entreprise ou Ecole du supérieur</strong>: ${clientType}</p>
-      <p><strong>Adresse</strong>: ${address}</p>
-      <p><strong>Services demandés</strong>:${servicesContent}</p>
-      `,
-    })
-    .then(() =>
-      res.status(200).json({
-        fr: "Votre demande a été envoyée avec succès",
-        en: "Your email has been successfully sent",
-      })
-    )
-    .catch(() =>
-      res.json({
-        fr: "Une erreur s'est produite, veuillez réessayer",
-        en: "An error occured, please try again",
-      })
-    );
-});
 
 module.exports = router;
